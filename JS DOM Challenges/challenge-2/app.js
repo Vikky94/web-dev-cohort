fa4d6ce6090f7baaa73084876ed96d54a0e866f1
Vikky <developer.vikky@gmail.com>
2025-02-26 21:06:30 +0530

    Challenge-2 Changes completed

--- JS DOM Challenges/challenge-2/app.js
+++ JS DOM Challenges/challenge-2/app.js
@@ -1,3 +1,10 @@
-/**
- * Write your challenge solution here
- */
+
+const buttons = document.getElementsByClassName("btns"),
+mainHeading = document.getElementById("mainHeading");
+
+for(let i=0; i< buttons.length; i++){
+    const button = buttons[i];
+    button.addEventListener('click',function(e){
+        mainHeading.style.color =  `#${e.target.getAttribute('data-color')}`;
+    })
+}
