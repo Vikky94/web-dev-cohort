d715f3fa365d1040b6c129c80f0feccfe2662289
Vikky <developer.vikky@gmail.com>
2025-02-27 00:09:34 +0530

    Challenge-3 Changes

--- JS DOM Challenges/challenge-3/app.js
+++ JS DOM Challenges/challenge-3/app.js
@@ -1,3 +1,12 @@
-/**
- * Write your challenge solution here
- */
+const typeFelds = document.getElementsByClassName('ontype');
+
+
+for (let i = 0; i < typeFelds.length; i++) {
+    const textFeild = typeFelds[i];
+
+    textFeild.addEventListener('keyup', (e) => {
+        const value = e.target.value,
+            targetFieldId = e.target.getAttribute('data-displayType');
+        document.getElementById(targetFieldId).innerText = value;
+    })
+}
