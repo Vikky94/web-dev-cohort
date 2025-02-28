5b5d27730920687bfd707ff015f32e1ef8a2ac3c
Vikky <developer.vikky@gmail.com>
2025-02-28 13:35:09 +0530

    Challenge-5 completed

--- JS DOM Challenges/challenge-5/app.js
+++ JS DOM Challenges/challenge-5/app.js
@@ -20,3 +20,105 @@ const images = [
     caption: 'Urban City Skyline',
   },
 ];
+
+const carouselTrack = document.getElementById("carouselTrack"),
+  caption = document.getElementById("caption"),
+  prevButton = document.getElementById("prevButton"),
+  nextButton = document.getElementById("nextButton"),
+  autoPlayButton = document.getElementById("autoPlayButton"),
+  timerDisplay = document.getElementById("timerDisplay"),
+  carouselNav = document.getElementById("carouselNav");
+images.forEach((img, i) => {
+  const imgTag = document.createElement('img'),
+    pTag = document.createElement('p'),
+    span = document.createElement('span');
+  span.setAttribute('data-id', i);
+  span.classList.add('carousel-indicator');
+  if (i === 0) {
+    imgTag.classList.add('carousel-slide');
+    pTag.classList.add('carousel-caption');
+    pTag.innerText = img.caption;
+    caption.appendChild(pTag);
+    span.classList.add('active');
+  }
+  imgTag.setAttribute('data-id', i);
+  imgTag.setAttribute('src', img.url);
+  imgTag.setAttribute('alt', img.caption);
+  carouselTrack.appendChild(imgTag);
+  carouselNav.appendChild(span);
+
+  span.addEventListener('click', function (e) {
+    const id = e.target.getAttribute('data-id');
+    if (!e.target.classList.contains('active')) {
+      carousel(Number(id));
+    }
+  });
+});
+
+
+nextButton.addEventListener('click', function () {
+  const currentItemId = Number(carouselTrack.children[0].getAttribute('data-id'));
+  const id = (currentItemId === images.length - 1) ? 0 : Number(currentItemId) + 1;
+  carousel(Number(id));
+});
+prevButton.addEventListener('click', function () {
+  next()
+});
+
+function next(){
+  const currentItemId = Number(carouselTrack.children[0].getAttribute('data-id'));
+  const id = (currentItemId === 0) ? images.length - 1 : Number(currentItemId) - 1;
+  carousel(Number(id));
+}
+
+let intervalTimeRefrence, secondInterValRef;
+let slideCounter = 5;
+autoPlayButton.addEventListener('click',function(e){
+  if( e.target.innerText === "Start Auto Play" ){
+    e.target.innerText = "Stop Auto Play";
+    timerDisplay.innerText = `Next slide in ${slideCounter}s`;
+
+    intervalTimeRefrence = setInterval(function(){
+      slideCounter=5;
+      timerDisplay.innerText = `Next slide in ${slideCounter}s`;
+      clearInterval(secondInterValRef);
+      next()
+      secondInterValRef = setInterval(function(){
+        timerDisplay.innerText = `Next slide in ${--slideCounter}s`;
+      }, 1000);
+    }, 5000);
+
+    secondInterValRef = setInterval(function(){
+      timerDisplay.innerText = `Next slide in ${--slideCounter}s`;
+    }, 1000);
+
+  }else{
+    e.target.innerText = "Start Auto Play";
+    timerDisplay.innerText = ``;
+    clearInterval(intervalTimeRefrence);
+    clearInterval(secondInterValRef);
+  }
+  
+});
+
+function carousel(id) {
+  console.log(`id ->>> ${id}`)
+  for (let i = 0; i < carouselTrack.children.length; i++) {
+    carouselTrack.children[i].classList.remove('carousel-slide');
+  }
+
+  let appendImgFirst;
+  for (let i = 0; i < carouselTrack.children.length; i++) {
+    const dataId = Number(carouselTrack.children[i].getAttribute('data-id'));
+    console.log(`${id} === ${dataId}  => ${id === dataId}`)
+    if (id === dataId) appendImgFirst = carouselTrack.children[i];
+  }
+  appendImgFirst.classList.add('carousel-slide');
+  caption.children[0].innerText = images[id].caption;
+  carouselTrack.insertBefore(appendImgFirst, carouselTrack.firstChild);
+
+  for (let i = 0; i < carouselNav.children.length; i++) {
+    carouselNav.children[i].classList.remove('active');
+  }
+  carouselNav.children[id].classList.add('active');
+}
